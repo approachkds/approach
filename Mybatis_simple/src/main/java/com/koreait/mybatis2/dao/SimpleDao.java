fed6a8bacf27ba45c9f59354787ec8524b41f1fb
approachkds <approachkds@naver.com>
2021-03-30 22:49:52 +0900

    create simpleUpdate / simpleDelete job!

--- Mybatis_simple/src/main/java/com/koreait/mybatis2/dao/SimpleDao.java
+++ Mybatis_simple/src/main/java/com/koreait/mybatis2/dao/SimpleDao.java
@@ -15,4 +15,9 @@ public interface SimpleDao {
 	
 	public SimpleDto simpleView(int no);
 	
+	// 수정 후 성공/실패 처리
+	public int simpleUpdate(String title, String content, int no);
+	
+	// 삭제 후 성공/실패 처리
+	public int simpleDelete(int no);
 }
