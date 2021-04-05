55bbb6f4a1c9ffc73bd6a2f5fff21beee008a4f9
approachkds <approachkds@naver.com>
2021-04-05 23:20:30 +0900

    iaccess 0405_sample!!

---
+++ DI_step01/src/iacess/di/DiExam.java
@@ -0,0 +1,29 @@
+package iacess.di;
+
+import iacess.di.entity.Hello;
+import iacess.di.ui.ColorPrinter;
+import iacess.di.ui.MonoPrinter;
+import iacess.di.ui.Printer;
+
+public class DiExam {
+
+	public static void main(String[] args) {
+		
+		/* constructor 방식*/
+		/*
+		//Printer printer = new ColorPrinter();
+		Printer printer = new MonoPrinter();
+		Hello hello = new Hello(printer);
+		hello.setPrinter(printer);
+		hello.print("Hello");
+		*/
+		
+		// set 방식
+		//Printer printer = new MonoPrinter();
+		Printer printer = new ColorPrinter();
+		Hello hello = new Hello();
+		hello.setPrinter(printer);
+		hello.print("hello");
+	}
+	
+}
